a43011f5f05279108fe03889e17f6645f75deafb
ytang913 <ytang18@terpmail.umd.edu>
2021-03-11 20:33:35 -0800

    changing the theme color

--- script.js
+++ script.js
@@ -16,11 +16,11 @@ for(var i = 0; themeDots.length > i; i++){
     })
 }
 function setTheme(mode){
-    if(mode == 'light'){
-        document.getElementById('theme-style').href = "default.css"
-    }
     if(mode == 'blue'){
-        document.getElementById('theme-style').href = "blue.css"
+        document.getElementById('theme-style').href = "main.css"
+    }
+    if(mode == 'pink'){
+        document.getElementById('theme-style').href = "pink.css"
     }
     if (mode == 'green'){
         document.getElementById('theme-style').href="green.css"
